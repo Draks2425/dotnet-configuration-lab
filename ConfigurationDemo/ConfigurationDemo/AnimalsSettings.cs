c8b2acbe705bf86cbb3a17c2e8ee648a2f8f8112
Kacper Kraus <kacperkraus2425@gmail.com>
2025-12-01 12:05:41 +0100

    Implement Options Pattern with DI

---
+++ ConfigurationDemo/ConfigurationDemo/AnimalsSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationDemo
+{
+    public class AnimalsSettings
+    {
+        //Section "Animals"in JSON
+        public AnimalDetail Cat { get; set; }
+        public AnimalDetail Dog { get; set; }
+        public string DefaultAnimalName { get; set; }
+    }
+
+    public class AnimalDetail
+    {
+        //This fits the property inside "Cat" and "Dog"
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int DailyFoodGrams { get; set; }
+    }
+}
